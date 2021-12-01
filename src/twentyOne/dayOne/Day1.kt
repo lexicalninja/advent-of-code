236d972018ee628eb73cadca708f0cad21b742ff
Patrick Saxton <patrick@urban-intellect.com>
2021-11-30 23:30:08 -0600

    format

--- src/twentyOne/dayOne/Day1.kt
+++ src/twentyOne/dayOne/Day1.kt
@@ -4,12 +4,12 @@ import java.io.File
 
 fun main(args: Array<String>) {
     partOne()
-   partTwo()
+    partTwo()
 }
 
 fun partOne() {
     val depths = mutableListOf<Int>()
-    File("src/twentyOne/dayOne/input.txt").useLines { lines -> lines.forEach { depths.add(it.toInt()) }}
+    File("src/twentyOne/dayOne/input.txt").useLines { lines -> lines.forEach { depths.add(it.toInt()) } }
     var count = 0
     depths.forEachIndexed { index, i -> if (index > 0 && depths[index - 1] < i) count++ }
     println(count)
@@ -18,7 +18,7 @@ fun partOne() {
 fun partTwo() {
     val depths = mutableListOf<Int>()
     var count = 0
-    File("src/twentyOne/dayOne/input.txt").useLines { lines -> lines.forEach { depths.add(it.toInt()) }}
+    File("src/twentyOne/dayOne/input.txt").useLines { lines -> lines.forEach { depths.add(it.toInt()) } }
     val windows = depths.windowed(3, 1, false).map { it.sum() }
     windows.forEachIndexed { index, i -> if (index > 0 && windows[index - 1] < i) count++ }
     println(count)
