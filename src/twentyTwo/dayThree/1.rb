5077634a33742ab34a306088bfe0d48253f2774c
Patrick Saxton <patrick@urban-intellect.com>
2022-12-05 13:35:04 -0600

    wip

--- src/twentyTwo/dayThree/1.rb
+++ src/twentyTwo/dayThree/1.rb
@@ -4,7 +4,6 @@ file = File.open("dayThree/input.txt")
 acc = 0
 file.readlines.each { |s|
   a = s.chomp.unpack("a#{s.chomp.length/2}a*")
-  r = a[0].split("").map { |s|s.ord > 96 ? s.ord - 96 : s.ord - 38 }
   b = Set[*a[0].split("").map { |s|s.ord > 96 ? s.ord - 96 : s.ord - 38 }]
   c = Set[*a[1].split("").map { |s|s.ord > 96 ? s.ord - 96 : s.ord - 38  }]
   acc += b.intersection(c).to_a.sum
